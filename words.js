88fe6a94d56fbf32f3247808ca3e6d3775d5660f
Samim Islam <110116844+Its-SK@users.noreply.github.com>
2026-05-04 21:55:28 +0530

    Update words.js

--- words.js
+++ words.js
@@ -1,29 +1,70 @@
 // words.js
 module.exports = [
-    // Animals
-    "alligator", "ant", "bear", "bee", "bird", "camel", "cat", "cheetah", "chicken", "cow", 
-    "crocodile", "deer", "dog", "dolphin", "duck", "eagle", "elephant", "fish", "fly", "fox", 
-    "frog", "giraffe", "goat", "gorilla", "hippo", "horse", "kangaroo", "lion", "monkey", "mouse", 
-    "octopus", "ostrich", "owl", "panda", "parrot", "penguin", "pig", "rabbit", "rhino", "seal", 
-    "shark", "sheep", "snail", "snake", "spider", "squirrel", "tiger", "turtle", "whale", "wolf", "zebra",
+    // --- ANIMALS & INSECTS ---
+    "alligator", "alpaca", "ant", "anteater", "armadillo", "baboon", "badger", "bat", "bear", "beaver", 
+    "bee", "beetle", "bird", "buffalo", "butterfly", "camel", "cat", "caterpillar", "catfish", "cheetah", 
+    "chicken", "chimpanzee", "cobra", "crab", "crocodile", "deer", "dinosaur", "dog", "dolphin", "donkey", 
+    "dragonfly", "duck", "eagle", "elephant", "emu", "flamingo", "fly", "fox", "frog", "giraffe", 
+    "goat", "goose", "gorilla", "grasshopper", "hamster", "hedgehog", "hippo", "horse", "hyena", "iguana", 
+    "jellyfish", "kangaroo", "koala", "ladybug", "leopard", "lion", "llama", "lobster", "monkey", "moose", 
+    "mosquito", "mouse", "octopus", "ostrich", "otter", "owl", "panda", "panther", "parrot", "peacock", 
+    "pelican", "penguin", "pig", "pigeon", "porcupine", "rabbit", "raccoon", "rat", "rhino", "rooster", 
+    "scorpion", "seagull", "seahorse", "seal", "shark", "sheep", "skunk", "sloth", "snail", "snake", 
+    "spider", "squid", "squirrel", "starfish", "swan", "tiger", "toad", "turkey", "turtle", "walrus", 
+    "whale", "wolf", "worm", "zebra",
 
-    // Food
-    "apple", "bacon", "banana", "bread", "broccoli", "burger", "cake", "candy", "carrot", "cheese", 
-    "cherry", "chocolate", "coffee", "cookie", "corn", "cupcake", "donut", "egg", "french fries", "grapes", 
-    "hamburger", "hot dog", "ice cream", "lemon", "milk", "mushroom", "onion", "pancake", "peanut", "pear", 
-    "pie", "pineapple", "pizza", "popcorn", "potato", "pretzel", "sandwich", "strawberry", "sushi", "taco", 
-    "tomato", "waffle", "watermelon",
+    // --- FOOD & DRINK ---
+    "apple", "avocado", "bacon", "banana", "beans", "blueberry", "bread", "broccoli", "burger", "burrito", 
+    "butter", "cabbage", "cake", "candy", "carrot", "cereal", "cheese", "cherry", "chocolate", "coconut", 
+    "coffee", "cookie", "corn", "croissant", "cucumber", "cupcake", "donut", "egg", "eggplant", "french fries", 
+    "garlic", "grapes", "hamburger", "honey", "hot dog", "ice cream", "ketchup", "kiwi", "lemon", "lettuce", 
+    "lollipop", "macaroni", "mango", "marshmallow", "meatball", "melon", "milk", "muffin", "mushroom", "mustard", 
+    "noodle", "onion", "orange", "pancake", "pasta", "peach", "peanut", "pear", "peas", "pepper", 
+    "pie", "pineapple", "pizza", "plum", "popcorn", "popsicle", "potato", "pretzel", "pumpkin", "radish", 
+    "raspberry", "rice", "salad", "salmon", "salt", "sandwich", "sausage", "soup", "spaghetti", "steak", 
+    "strawberry", "sushi", "taco", "tea", "toast", "tomato", "waffle", "walnut", "watermelon",
 
-    // Objects & Things
-    "airplane", "ambulance", "backpack", "balloon", "bed", "bicycle", "boat", "book", "bus", "camera", 
-    "car", "chair", "clock", "computer", "couch", "cup", "door", "drum", "envelope", "fire engine", 
-    "glasses", "guitar", "hammer", "helicopter", "key", "kite", "knife", "lamp", "laptop", "motorcycle", 
-    "pencil", "phone", "piano", "scissors", "ship", "shoe", "spoon", "submarine", "sword", "table", 
-    "telescope", "television", "tent", "train", "truck", "umbrella", "watch", "window",
+    // --- OBJECTS & ITEMS ---
+    "alarm clock", "anchor", "anvil", "armor", "arrow", "axe", "backpack", "balloon", "band-aid", "barrel", 
+    "basket", "bathtub", "battery", "bed", "bell", "belt", "blanket", "book", "boomerang", "boots", 
+    "bottle", "bow", "bowl", "box", "bracelet", "brick", "broom", "brush", "bucket", "button", 
+    "calculator", "calendar", "camera", "candle", "cannon", "cap", "cards", "carpet", "chair", "chalk", 
+    "chandelier", "clock", "coat", "comb", "compass", "computer", "couch", "crayons", "crown", "cup", 
+    "diamond", "dice", "door", "dress", "drum", "dynamite", "earrings", "envelope", "eraser", "feather", 
+    "fence", "flag", "flashlight", "flute", "fork", "fridge", "gem", "glasses", "globe", "glove", 
+    "guitar", "hammer", "hat", "headphones", "helmet", "hook", "horn", "hourglass", "jacket", "jeans", 
+    "key", "keyboard", "kite", "knife", "ladder", "lamp", "laptop", "laser", "lasso", "lightbulb", 
+    "lock", "magnet", "magnifying glass", "map", "mask", "match", "microphone", "mirror", "money", "monitor", 
+    "mop", "mousepad", "mug", "nail", "necklace", "needle", "notebook", "oven", "pacifier", "padlock", 
+    "paint", "paintbrush", "pants", "paper", "parachute", "pencil", "perfume", "phone", "piano", "picture", 
+    "pillow", "pipe", "plate", "plug", "pocket", "poison", "purse", "puzzle", "pyramid", "quilt", 
+    "radio", "ring", "robot", "rope", "ruler", "sack", "saddle", "scissors", "screw", "shield", 
+    "shirt", "shoe", "shovel", "sink", "skateboard", "skates", "skirt", "soap", "socks", "sofa", 
+    "spear", "sponge", "spoon", "spring", "stairs", "stamp", "statue", "stethoscope", "stove", "suitcase", 
+    "sunglasses", "sword", "syringe", "table", "target", "teapot", "teddy bear", "telephone", "telescope", "television", 
+    "tent", "thermometer", "throne", "tie", "tire", "toaster", "toilet", "toothbrush", "toothpaste", "torch", 
+    "towel", "toy", "tractor", "trash can", "treasure", "treehouse", "trophy", "trumpet", "umbrella", "vacuum", 
+    "vase", "violin", "wallet", "watch", "weapon", "web", "wheel", "whistle", "window", "yarn",
 
-    // Places, Nature & Fantasy
-    "alien", "astronaut", "beach", "bridge", "castle", "cloud", "diamond", "dragon", "earth", "fire", 
-    "flower", "forest", "ghost", "grass", "hospital", "house", "island", "lightning", "mermaid", "moon", 
-    "mountain", "ocean", "rain", "rainbow", "river", "robot", "rocket", "school", "snow", "snowman", 
-    "star", "sun", "tornado", "tree", "unicorn", "vampire", "volcano", "zombie"
+    // --- NATURE, PLACES & ENVIRONMENT ---
+    "alien", "asteroid", "astronaut", "avalanche", "beach", "black hole", "blizzard", "bridge", "camp", "canyon", 
+    "castle", "cave", "church", "city", "cliff", "cloud", "comet", "crater", "desert", "dirt", 
+    "earth", "earthquake", "farm", "fire", "flower", "forest", "galaxy", "garden", "glacier", "grass", 
+    "hill", "hospital", "hotel", "house", "hurricane", "ice", "igloo", "island", "jungle", "lake", 
+    "leaf", "lightning", "meteor", "moon", "mountain", "mud", "ocean", "park", "planet", "plant", 
+    "puddle", "rain", "rainbow", "river", "road", "rock", "sand", "school", "sky", "smoke", 
+    "snow", "snowflake", "snowman", "space", "star", "storm", "sun", "sunflower", "swamp", "tornado", 
+    "tree", "tsunami", "universe", "vine", "volcano", "water", "waterfall", "wave", "wind", "world",
+
+    // --- VEHICLES & TRANSPORT ---
+    "airplane", "ambulance", "bicycle", "blimp", "boat", "bus", "cable car", "canoe", "car", "carriage", 
+    "cart", "excavator", "firetruck", "golf cart", "helicopter", "hovercraft", "jeep", "jet", "kayak", "limousine", 
+    "motorcycle", "police car", "raft", "rocket", "sailboat", "scooter", "ship", "spaceship", "submarine", "subway", 
+    "taxi", "train", "tricycle", "truck", "ufo", "unicycle", "van", "wagon", "yacht", "zeppelin",
+
+    // --- ACTIONS & VERBS (Fun to draw!) ---
+    "baking", "boxing", "crying", "dancing", "diving", "drawing", "drinking", "eating", "falling", "fishing", 
+    "flying", "hugging", "juggling", "jumping", "kicking", "kissing", "laughing", "listening", "painting", "playing", 
+    "reading", "running", "singing", "skating", "skiing", "sleeping", "smiling", "sneezing", "snoring", "surfing", 
+    "swimming", "talking", "thinking", "throwing", "walking", "waving", "writing", "yawning"
 ];
